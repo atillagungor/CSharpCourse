836d5e48fb386d98da6ca38b361be3e96a03542d
Atilla Güngör <atillaagungorr@gmail.com>
2023-10-23 18:36:40 +0300

    Methods eklendi

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Add();
+Add2(8, 10);
+static void Add()
+{
+    Console.WriteLine("Added");
+}
+
+static int Add2(int number1, int number2)
+{
+    var result = number1 + number2;
+    Console.WriteLine("Sonuç: " + result);
+    return result;
+}
+Console.ReadLine();
