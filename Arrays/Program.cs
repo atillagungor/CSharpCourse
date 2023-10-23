989f61e0ec155cd01449377b9a9fad6532f32b5f
Atilla Güngör <atillaagungorr@gmail.com>
2023-10-24 00:39:16 +0300

    Array projesi eklendi & bölüm 28 tamamlandı

---
+++ Arrays/Program.cs
@@ -0,0 +1,14 @@
+﻿string [] students = new string[3];
+students[0] = "Engin";
+students[1] = "Atilla";
+students[2] = "Umut";
+
+string[] students2 = {"Engin","Atilla","Umut"};
+
+foreach (var student in students2)
+{
+    Console.WriteLine(student);
+}
+
+Console.WriteLine();
+Console.ReadLine();
