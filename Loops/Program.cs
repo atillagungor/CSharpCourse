6ac095790a7f3cefd0d5b5da3826b127593e6e22
Atilla Güngör <atillaagungorr@gmail.com>
2023-10-24 15:03:33 +0300

    Loops projesi eklendi.

---
+++ Loops/Program.cs
@@ -0,0 +1,47 @@
+﻿
+//DÖNGÜLER
+//FOR
+
+
+//for (int i = 100; i >= 0; i=i-2)
+//{
+//    Console.WriteLine(i);
+//}
+//Console.WriteLine("Finished!");
+
+
+
+
+//WHILE
+
+//int number = 100;
+//while (number >= 0)
+//{
+//    Console.WriteLine(number);
+//    number--;
+//}
+//Console.WriteLine("Now number is {0}", number);
+//Console.ReadLine();
+
+//DO WHILE
+
+
+//int number = 10;
+//do
+//{
+//    Console.WriteLine(number);
+//    number--;
+
+//} while (number >= 0);
+
+//Console.ReadLine();
+
+
+//FOREACH
+
+string[] students = new string[3] { "Engin", "Atilla", "Umut" };
+foreach (var student in students)
+{
+    Console.WriteLine(student);
+}
+Console.ReadLine();
