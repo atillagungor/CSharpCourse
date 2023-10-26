c75b112d1032cf2cbb3f132756881ba6293b8036
Atilla Güngör <atillaagungorr@gmail.com>
2023-10-26 18:12:00 +0300

    Kapsülleme bölümü eklendi.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.ExceptionServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Customer customer = new Customer();
+        Student student = new Student();
+        student.Departmant = "Software";
+
+        Person[] person = new Person[3]
+        {
+            new Customer
+            {FirstName =  "Atilla" },
+            new Student
+            {FirstName = "Arda"},
+            new Person
+            {FirstName = "Engin"}
+        }; 
+        foreach (var item in person)
+        {
+            Console.WriteLine(item.FirstName);
+        }
+
+        Console.ReadLine();
+    }
+}
+
+
+class Person
+{
+    public int Id {  get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+class Customer:Person
+{
+    public string City { get; set; }
+}
+
+class Student : Person
+{
+    public string Departmant { get; set; }
+}
